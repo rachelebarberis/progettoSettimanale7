756c1ec7847553a41e4538e0219f3365e4fa60dc
Rachele Barberis <rachele.barberis.12@gmail.com>
2024-12-13 17:58:34 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-const api_url = 'https://striveschool-api.herokuapp.com/api/product/';
+const api = 'https://striveschool-api.herokuapp.com/api/product/';
 const myUrl = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzVhOWVmNTk3ZTI5ZjAwMTVjMmU2OWMiLCJpYXQiOjE3MzM5OTIxODEsImV4cCI6MTczNTIwMTc4MX0.QQRgxGrH9GlZ78fRXz_q6HAfzlO4n9VODTsUPrCzOKs";
 const headers = {
     "Authorization" : myUrl,
     "Content-Type": "application/json",
 }
 const productContainer =document.getElementById('productContainer');
-const myForm =document.getElementById('product-form');
+const productForm =document.getElementById('product-form');
 const productName =document.getElementById('productName');
 const productBrand = document.getElementById('productBrand');
 const productPrice = document.getElementById('productPrice');
@@ -28,63 +28,76 @@ class product {
 let newProduct;
 
 
+
+
 document.addEventListener('load', init());
 
 function init() {
-    btnSave.setAttribute('disabled', 'true');
     readProducts();
 }
 
 
 
 async function readProducts() {
-    try{
-        let read=await fetch(api_url);
-        let response=await read.json();
-        productList=response;
-        if (productList.length>0) {
-            printProduct();
+    try {
+  
+        const response = await fetch(api, {
+            method: 'GET',
+            headers: headers,
+        });
+
+        if (response.ok) {
+            productForm.reset();
+            alert('Prodotto aggiunto con successo!');
         } else {
-            empty.innerText= 'Nessun prodotto presente';
-            return;
+            alert('Errore durante la creazione del prodotto.');
         }
     } catch (error) {
-        console.log('Errore nel recupero dei dati: ', error);
-        empty.innerText = `Errore nel recupero dei dati: ${error}`;
+        console.error('Errore durante la creazione del prodotto:', error);
+        alert('Errore durante la creazione del prodotto.');
     }
 };
+btnSave.addEventListener('submit', async (e) => {
+    e.preventDefault(); 
+
+    newProduct = new product (productName.value, productDescription.value, productBrand.value, productImg.value, parseInt(productPrice.value));
+});
 
 
-const printProduct= () => {
-    empty.innerText='';
-    productList.forEach((product)=> {
-        const col =document.createElement('div');
+
+
+
+
+function printProduct() {
+    empty.innerText = '';
+    productList.forEach((product) => {
+        const col = document.createElement('div');
         col.classList.add('col-sm-6 col-md-4, col-lg-4');
 
-        const card=document.createElement('div');
+        const card = document.createElement('div');
         card.classList.add('card', 'mb-4', 'shadow-sm');
 
-        const img=document.createElement('img');
+        const img = document.createElement('img');
         img.src = product.imgageUrl;
         img.classList.add('bd-placeholder-img', 'card-img-top');
 
         const cardBody = document.createElement('div');
         cardBody.classList.add('card-body');
 
-        const title =document.createElement('h4');
+        const title = document.createElement('h4');
         title.classList.add('card-title');
-        title.innerText= product.name;
+        title.innerText = product.name;
 
         const cardDescription = document.createElement('p');
-        cardDescription.innerText= product.description;
+        cardDescription.innerText = product.description;
 
-        const btn1= document.createElement('button');
+        const btn1 = document.createElement('button');
         btn1.setAttribute('type', 'button');
         btn1.textContent = 'Modifica';
         btn1.classList.add('btn', 'btn-sm', 'btn-outline-secondary');
 
 
-        const btn2= document.createElement('button');
+        const btn2 = document.createElement('button');
         btn2.setAttribute('type', 'button');
         btn2.textContent = 'Scopri di piu';
         btn2.classList.add('btn', 'btn-sm', 'btn-outline-secondary');
@@ -97,6 +110,6 @@ const printProduct= () => {
         card.appendChild(cardBody);
         col.appendChild(card);
         productContainer.appendChild(col);
-    })
-};
+    });
+}
 
